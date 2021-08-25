2ca4763584a9b9e0a867ef0b26009d550f086503
rurigk <rurigk@gmail.com>
2021-08-25 15:45:55 -0500

    Moved Article as mod and implement serde

--- src/rpublish/article.rs
+++
@@ -1,8 +0,0 @@
-pub struct Article{
-    title: String,
-    author: String,
-    data: String,
-    tags: Vec<String>,
-    created_date: DateTime<Utc>,
-    update_date: DateTime<Utc>
-}
---
+++ src/rpublish/articles_manager/article.rs
@@ -0,0 +1,12 @@
+use chrono::{DateTime, Utc};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Article{
+    pub title: String,
+    pub author: String,
+    pub data: String,
+    pub tags: Vec<String>,
+    pub created_date: DateTime<Utc>,
+    pub update_date: DateTime<Utc>
+}
