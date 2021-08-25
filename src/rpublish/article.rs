a33c15173cd11421bb65f52f27c7bc534b7137fe
rurigk <rurigk@gmail.com>
2021-08-25 14:34:35 -0500

    First definition of article data structure

--- src/rpublish/article.rs
+++ src/rpublish/article.rs
@@ -1,1 +1,8 @@
-pub struct Article{}
+pub struct Article{
+    title: String,
+    author: String,
+    data: String,
+    tags: Vec<String>,
+    created_date: DateTime<Utc>,
+    update_date: DateTime<Utc>
+}
